31292f1ee8c26a52f267b1ec3c24e3cbb1dcd7b8
My Tan <mingyi.25@intl.zju.edu.cn>
2026-03-26 10:37:57 +0800

    adding output comment

--- Practical4/Variables.py
+++ Practical4/Variables.py
@@ -1,33 +1,37 @@
 #Variables.py
 #estimated population of Scotland in 2004(a),2014(b),2024(c)
-#the change in population between 2004 and 2014(d)
-#the change in population between 2014 and 2024(e)
 
 a=5.08*10**6
 b=5.33*10**6
 c=5.55*10**6
 
+#the change in population between 2004 and 2014(d)
 d=b-a
-print("%.0f" %d) #float: 0f,zero declimal point; 1f, one declimal point
+print("the change in population between 2004 and 2014 is %.0f" %d) #float: 0f,zero declimal point; 1f, one declimal point
 
+#the change in population between 2014 and 2024(e)
 e=c-b
-print("%.0f"%e)
+print("the change in population between 2014 and 2024 is %.0f"%e)
 
+#compare d and e
 if d < e :
     print ("The change in population between 2014 and 2024 is larger. It is accelerating.")
 else :
     print ("The change in population between 2004 and 2014 is larger. It is decelerating.")
 
-
-#compare d and e
 #Larger:d
 #Population Growth: Decelarating
 
 #Booleans:
 X=True
 Y=False
-w=X or Y
-print(w)
+w= X or Y, X or X, Y or X, Y or Y
+print("Truth table:", w)
 
+#Output: 
+# True or False = True
+# True or True = True
+# False or True = True
+# False or False = False
 
 
