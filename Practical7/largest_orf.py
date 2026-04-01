f6bae187e6bf515e8783fc01d32c05374d9d11ac
My Tan <mingyi.25@intl.zju.edu.cn>
2026-04-01 14:16:33 +0800

    second script

--- Practical7/largest_orf.py
+++ Practical7/largest_orf.py
@@ -1,19 +1,42 @@
-#write	code	to	identify	the	longest	potential	open	reading	in within	gene	sequences.	
+# Open Reading Frames (ORF)
+# search the longest chain
+
 seq = 'AAGAUACAUGCAAGUGGUGUGUCUGUUCUGAGAGGGCCUAAAAG'
 
-#Part1: Identify the largest Open Reading Frames and report the length of it
+# search the start point AUG, no space, 3 as a group
+# start codon begin at anywhere
 import re
-longest_orf1= re.search ("^AUG.*UAA$",seq)
-longest_orf2= re.search ("^AUG.*UAG$",seq)
-longest_orf3= re.search ("^AUG.*UGA$", seq)
+targets = ["AUG", "UAA", "UGA", "UAG"]
 
-#if longest_orf1:
-#    print (longest_orf1)
-#elif longest_orf2:
-#    print (longest_orf2)
-#else:
-#    print (longest_orf3)
+for codon in targets:
+    match = re.search(f"({codon})", seq) 
+    
+    if match:
+        print("Codon exist:",match)
+    else:
+        print("none\n") # create a newline (/n)
 
-print (longest_orf3)
-print (longest_orf2)
-print (longest_orf1)
+# UAA, UGA exist, but UAG doesn't exist
+
+# Logic: find AUG, stop at UAG
+#   if there is UAG, print the output
+#	else run again and stop at UAA
+#	else run again and stop at UGA
+
+result_UAA = re.findall(r"AUG.*UAA", seq)
+
+result_UGA = re.findall(r"AUG.*UGA", seq)
+
+# If len(UGA) > len(UAA):
+# print "largest:" (UGA) and len(UGA)
+# else print UAA using same printing format.
+[chain1]= result_UAA
+[chain2]= result_UGA
+
+if len(chain1) > len(chain2):
+	print("longest:",chain1)
+	print("length:", len(chain1))
+else:
+    print("longest:", chain2)
+    print("length:", len(chain2))
+
